2e2588ef8010aa8eb47da1768ce2266509c001c2
OverLord <smouk.chayz@gmail.com>
2018-10-23 22:33:07 +0300

    Add sql-init of creation_time for messages

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,15 +20,15 @@ INSERT INTO dialog (id, interlocutor_one_id, interlocutor_two_id) VALUES
 (2001, 1002, 1003),
 (2002, 1000, 1004);
 
-INSERT INTO message (id, user_id, dialog_id, text) VALUES
-(3000, 1000, 2000, 'hello'),
-(3001, 1001, 2000, 'hello you'),
-(3002, 1000, 2000, 'tell me a joke'),
-(3003, 1000, 2000, 'can you'),
-(3004, 1001, 2000, 'of course'),
-(3005, 1001, 2000, 'tuk tuk'),
-(3006, 1000, 2000, 'who is where?'),
-(3007, 1003, 2001, 'hello new'),
-(3008, 1000, 2002, 'hello 1004'),
-(3009, 1004, 2002, 'hello user');
+INSERT INTO message (id, user_id, dialog_id, text, creation_time) VALUES
+(3000, 1000, 2000, 'hello', '2018-10-23 20:00:00'),
+(3001, 1001, 2000, 'hello you', '2018-10-23 20:01:00'),
+(3002, 1000, 2000, 'tell me a joke', '2018-10-23 20:02:00'),
+(3003, 1000, 2000, 'can you', '2018-10-23 20:03:00'),
+(3004, 1001, 2000, 'of course', '2018-10-23 20:04:00'),
+(3005, 1001, 2000, 'tuk tuk', '2018-10-23 20:05:00'),
+(3006, 1000, 2000, 'who is where?', '2018-10-23 20:06:00'),
+(3007, 1003, 2001, 'hello new', '2018-10-23 20:07:00'),
+(3008, 1000, 2002, 'hello 1004', '2018-10-23 20:08:00'),
+(3009, 1004, 2002, 'hello user', '2018-10-23 20:09:00');
 
